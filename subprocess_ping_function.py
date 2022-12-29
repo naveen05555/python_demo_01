c635b1a351ddbb70d4e86bc96e14cbd2380b6d28
naveen05555 <naveen05555@gmail.com>
2022-12-29 10:45:28 +0530

    my first commit

---
+++ subprocess_ping_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import subprocess
+
+def check_ip(ip_address):
+	result = subprocess.run(['ping', '-c', '4', '-n', ip_address], 
+	stdout = subprocess.PIPE,
+	stderr = subprocess.PIPE,
+	encoding = 'utf-8')
+	
+	if  result.returncode == 0:
+		return True, result.stdout
+	else:
+		return False, result.stderr
+
+print(check_ip('10.64.86.1'))
+
+print(check_ip('a'))
